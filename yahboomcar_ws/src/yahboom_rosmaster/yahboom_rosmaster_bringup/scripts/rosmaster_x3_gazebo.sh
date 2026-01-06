bf4ff5f2dcf01807da3e9f609458d2c0b8bb2d38
Juan Francisco Sanchez <jfranciscosm99@gmail.com>
2026-01-06 10:59:47 -0500

    se agrego un nuevo mundo

--- yahboomcar_ws/src/yahboom_rosmaster/yahboom_rosmaster_bringup/scripts/rosmaster_x3_gazebo.sh
+++ yahboomcar_ws/src/yahboom_rosmaster/yahboom_rosmaster_bringup/scripts/rosmaster_x3_gazebo.sh
@@ -12,19 +12,20 @@ trap 'cleanup' SIGINT SIGTERM
 
 # For cafe.world -> z:=0.20
 # For house.world -> z:=0.05
+# For bookstore.world -> 0.0394,1.5786,0.05
 # To change Gazebo camera pose: gz service -s /gui/move_to/pose --reqtype gz.msgs.GUICamera --reptype gz.msgs.Boolean --timeout 2000 --req "pose: {position: {x: 0.0, y: -2.0, z: 2.0} orientation: {x: -0.2706, y: 0.2706, z: 0.6533, w: 0.6533}}"
 echo "Launching Gazebo simulation..."
 ros2 launch yahboom_rosmaster_gazebo yahboom_rosmaster.gazebo.launch.py \
     enable_odom_tf:=true \
     headless:=False \
     load_controllers:=true \
-    world_file:=house.world \
+    world_file:=bookstore.world \
     use_rviz:=true \
     use_robot_state_pub:=true \
     use_sim_time:=true \
-    x:=0.0 \
-    y:=0.0 \
-    z:=0.20 \
+    x:=0.0394 \
+    y:=1.5786\
+    z:=0.05 \
     roll:=0.0 \
     pitch:=0.0 \
     yaw:=0.0 &
